47d9a46594ffcbb81dbcb602a99982e99f6fe44c
bidrohi462 <shovan.livebd@yahoo.com>
2015-06-15 09:59:04 +0600

    Test heroku for parameter pass
    Test code for parameter passing in heroku

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
-echo "Minhajul Bashir";
+if(isset($_GET['request']) && !empty($_GET['request']))
+	echo $_GET['request'];
+if(isset($_GET['q']) && !empty($_GET['q']))
+	echo " ".$_GET['q'];
+echo " Minhajul Bashir";
